00c14b7915da77e55b3bb27159ca18550e90e0e7
okaydokay97 <rubens.fernandes97@gmail.com>
2020-05-17 21:19:21 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all_dogs = []
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all_dogs << self
+  end
+
+  def self.all
+    @@all_dogs 
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
+require'pry'
 class Owner
-  # code goes here
+attr_reader :name, :species
+attr_accessor :cat, :dog
+@@all_human = []
+@@all_pets = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@all_human << self
+  end
+
+  def self.all 
+    @@all_human
+  end
+
+  def self.count
+    self.all.count 
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def cats
+    Cat.all.select {|cat| cat.owner == self}
+  end
+
+  def dogs
+    Dog.all.select {|dog| dog.owner == self}
+  end
+
+  def buy_cat(cat)
+    Cat.new(cat, self)
+  end
+
+  def buy_dog(dog)
+    Dog.new(dog, self)
+  end
+
+  def walk_dogs
+    dogs.map {|dog|dog.mood = "happy"}
+  end
+
+  def feed_cats
+    cats.map {|cat|cat.mood = "happy"}
+  end
+
+  def sell_pets
+    cats.map do |cat|
+    cat.mood = "nervous" 
+    cat.owner = nil
+    end
+    dogs.map do |dog|
+    dog.mood = "nervous" 
+    dog.owner = nil
+    end
+  end
+
+  def list_pets
+    "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+  end
+
 end
